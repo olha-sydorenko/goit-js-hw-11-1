a3e38b74a519470e21472dda9e03c6facb2dbdce
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-18 13:54:47 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { PixabayAPI } from './PixabayAPI.js';
-import { Notify } from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const searchForm = document.querySelector('.search-form');
 const photosGallery = document.querySelector('.gallery');
@@ -7,69 +7,58 @@ const loadMoreBtn = document.querySelector('.load-more');
 
 const pixabayAPI = new PixabayAPI();
 
-
 const onSearchFormSubmit = async event => {
-    event.preventDefault();
-    
-    pixabayAPI.query = event.currentTarget.searchQuery.value;
-    pixabayAPI.page = 1;
+  event.preventDefault();
 
-    try {
+  pixabayAPI.query = event.currentTarget.searchQuery.value;
+  pixabayAPI.page = 1;
+  photosGallery.innerHTML = '';
+
+  try {
     const { data } = await pixabayAPI.searchPhotos();
-
-        if (!data.hits.length) {
-            alert('oops!')
-            event.target.reset();
-            photosGallery.innerHTML = '';
-            loadMoreBtn.classList.add('is-hidden');
-            return;
-        } else if ((data.hits.length * pixabayAPI.page) < 40) {
-        //alert("We're sorry, but you've reached the end of search results");
-        renderPhotos(data.hits);
-        console.log(data.hits.length);
-            console.log(data.totalHits);
-            console.log(data.hits.length * pixabayAPI.page);
-        }  else { 
-        renderPhotos(data.hits);
-        console.log(data);
-            loadMoreBtn.classList.remove('is-hidden');  
+    if (!data.hits.length) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      event.target.reset();
+      photosGallery.innerHTML = '';
+      loadMoreBtn.classList.add('is-hidden');
+      return;
+    } else if (data.hits.length < 40) {
+      renderPhotos(data.hits);
+      Notify.info("We're sorry, but you've reached the end of search results");
+    } else {
+      renderPhotos(data.hits);
+      Notify.info(`Hooray! We found ${data.totalHits} images`);
+      loadMoreBtn.classList.remove('is-hidden');
     }
-
-    } catch(error) {
-        console.log(error);
-    } 
+  } catch (error) {
+    console.log(error);
+  }
 };
-
 
 const onLoadBtnClick = async event => {
-    pixabayAPI.page += 1;
+  pixabayAPI.page += 1;
 
-try {
+  try {
     const { data } = await pixabayAPI.searchPhotos();
-        if ((data.hits.length * pixabayAPI.page) <= 40) {
-            //alert("We're sorry, but you've reached the end of search results");
-            renderPhotos(data.hits);
-            console.log(data.hits.length);
-                console.log(data.totalHits);
-                loadMoreBtn.classList.add('is-hidden');
-            } 
-    renderPhotos(data.hits);
-    console.log(data.hits.length);
-    console.log(data.totalHits);
-    console.log(data.hits.length * pixabayAPI.page);
-    
-} catch(error) {
+    if (data.hits.length < 40 || pixabayAPI.page === 13) {
+      Notify.info("We're sorry, but you've reached the end of search results");
+      renderPhotos(data.hits);
+      loadMoreBtn.classList.add('is-hidden');
+    } else {
+      renderPhotos(data.hits);
+      Notify.info(`Hooray! We found ${data.totalHits} images`);
+    }
+  } catch (error) {
     console.log(error);
-} 
+  }
 };
 
-
-
-
 function renderPhotos(arr) {
-
-     const markup = arr.map(({ webformatURL, tags, likes, views, comments, downloads}) => {
-return `<div class="photo-card">
+  const markup = arr
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
          <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
@@ -85,11 +74,39 @@ return `<div class="photo-card">
             <b>Downloads</b> ${downloads}
           </p>
         </div>
-      </div>`
-    }).join('');
-photosGallery.insertAdjacentHTML('beforeend', markup); 
+      </div>`;
+    })
+    .join('');
+  photosGallery.insertAdjacentHTML('beforeend', markup);
 }
 
+searchForm.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadBtnClick);
+
+function renderPhotos(arr) {
+  const markup = arr
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
+         <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b> ${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b> ${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b> ${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b> ${downloads}
+          </p>
+        </div>
+      </div>`;
+    })
+    .join('');
+  photosGallery.insertAdjacentHTML('beforeend', markup);
+}
 
 searchForm.addEventListener('submit', onSearchFormSubmit);
 loadMoreBtn.addEventListener('click', onLoadBtnClick);
